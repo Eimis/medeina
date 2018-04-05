4a076bad58b938579106d69586f0850f3bc112a5
Eimantas <eimantas.stonys@gmail.com>
2018-04-05 21:55:19 +0200

    IssueCategory name should be unique

--- medeina/medeina/models.py
+++ medeina/medeina/models.py
@@ -9,7 +9,7 @@ from django_states.fields import StateField
 
 @python_2_unicode_compatible
 class IssueCategory(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
